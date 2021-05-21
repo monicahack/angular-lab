b53993eca0b1e89dc09b4f603134b9d047078407
Monica Hackney <mhackney@HAGERTY.com>
2021-05-21 13:48:42 -0400

    added filter, strikethrough and complete button only shows up when the item is not complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -5,15 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent implements OnInit {
-  Todo = [
+  Todo: any[] = [
     {task:"fold clothes", completed:true},
     {task:"put clothes away", completed:false},
     {task:"cross stitch", completed:false},
     {task:"run", completed:true},
     {task:"make breakfast", completed:true},
     {task:"walk dog", completed:false},
+    {task:"paddle board", completed:true},
+    {task:"rest", completed:false},
+    {task:"clean floors", completed:false},
   ];
+
+  filterTodoInput = '';
+  addTodoInput = '';
+  searchText = '';
 
   constructor() { }
 
